8cba808f81d46bc059dac9845d79c7e2448e61bc
santiago-federici <santiagofederici88@gmail.com>
2024-08-19 07:13:45 -0300

    update: nav links

---
+++ src/lib/constants.ts
@@ -0,0 +1,22 @@
+export const NAV_LINKS = [
+  {
+    title: "Inicio",
+    href: "#inicio",
+  },
+  {
+    title: "Beneficios",
+    href: "#benefits",
+  },
+  {
+    title: "Servicios",
+    href: "#services",
+  },
+  {
+    title: "Quénes somos",
+    href: "#who-we-are",
+  },
+  {
+    title: "Empezar a invertir",
+    href: "#requirements",
+  },
+];
