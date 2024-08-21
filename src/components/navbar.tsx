84e984388d4561c36c7aef5d1a492864123f8322
santiago-federici <santiagofederici88@gmail.com>
2024-08-21 07:39:16 -0300

    update: navbar animation

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,17 +1,27 @@
 "use client";
 
-import { useState } from "react";
 import { NAV_LINKS } from "../lib/constants";
-import { AnimatePresence, motion } from "framer-motion";
+import { cn } from "../lib/utils";
+
+import { AnimatePresence, motion, useCycle, MotionConfig } from "framer-motion";
+
 import Button from "./button";
-import MenuIcon from "../icons/menu";
 
 export default function Navbar() {
-  const [open, setOpen] = useState(false);
+  const [menu, toggleMenu] = useCycle(false, true);
 
-  const toggleMenu = () => {
-    setOpen((prev) => !prev);
-  };
+  // disable scrolling when menu is open
+  // useEffect(() => {
+  //   if (menu) {
+  //     setTimeout(() => (document.body.style.overflow = "hidden"), 500);
+  //   } else {
+  //     document.body.style.overflow = "";
+  //   }
+
+  //   return () => {
+  //     document.body.style.overflow = "";
+  //   };
+  // }, [menu]);
 
   return (
     <header className="wrapper flex items-center justify-between py-4">
@@ -43,31 +53,66 @@ export default function Navbar() {
         </Button>
       </nav>
 
-      <div onClick={toggleMenu}>
-        <MenuIcon className="flex cursor-pointer lg:hidden" />
-      </div>
+      <motion.button
+        animate={menu ? "open" : "close"}
+        className={cn("z-50 flex flex-col gap-y-1 px-1 py-1.5 lg:hidden", {
+          "right-6 top-6 max-md:fixed": menu,
+        })}
+        onClick={() => toggleMenu()}
+      >
+        <motion.span
+          variants={{
+            open: {
+              rotate: 45,
+              y: 6,
+            },
+            close: {
+              rotate: 0,
+              y: 0,
+            },
+          }}
+          className="h-0.5 w-5 bg-black"
+        ></motion.span>
+        <motion.span
+          variants={{
+            open: {
+              rotate: 45,
+              opacity: 0,
+            },
+            close: {
+              rotate: 0,
+              opacity: 1,
+            },
+          }}
+          className="h-0.5 w-5 bg-black"
+        ></motion.span>
+        <motion.span
+          variants={{
+            open: {
+              rotate: -45,
+              y: -6,
+            },
+            close: {
+              rotate: 0,
+              y: 0,
+            },
+          }}
+          className="h-0.5 w-5 bg-black"
+        ></motion.span>
+      </motion.button>
 
       <AnimatePresence>
-        {open && (
-          <>
-            <motion.div
-              variants={{
-                initial: {
-                  opacity: 0,
-                },
-                animate: {
-                  opacity: 1,
-                },
-              }}
-              initial="initial"
-              animate="animate"
-              className="fixed inset-0 z-40 bg-black/50 lg:hidden"
-            ></motion.div>
-
+        {menu && (
+          <MotionConfig
+            transition={{
+              type: "spring",
+              bounce: 0,
+            }}
+          >
             <motion.aside
               variants={{
                 initial: {
-                  x: "500px",
+                  x: "100%",
                 },
                 animate: {
                   x: "0",
@@ -75,24 +120,32 @@ export default function Navbar() {
               }}
               initial="initial"
               animate="animate"
-              className="fixed right-0 top-0 z-50 flex h-full w-80 flex-col gap-4 border-l bg-background px-6 pt-7 shadow-lg lg:hidden"
+              exit="initial"
+              className="fixed inset-0 z-40 flex flex-col gap-4 bg-background px-6 py-7 shadow-lg md:px-16 lg:hidden"
             >
-              <div onClick={toggleMenu} className="ml-auto">
-                <MenuIcon className="cursor-pointer" />
-              </div>{" "}
-              <nav className="mt-8 flex w-60 flex-col items-start gap-4 lg:hidden">
+              <nav className="mt-8 flex flex-col items-start gap-6 lg:hidden">
                 {NAV_LINKS.map((link) => (
                   <a
                     key={link.href}
                     href={link.href}
-                    className="duration-200 hover:text-primary"
+                    className="text-2xl duration-200 hover:text-primary"
+                    onClick={() => toggleMenu()}
                   >
                     {link.title}
                   </a>
                 ))}
               </nav>
-              <Button>Clientes</Button>
-              <picture className="mx-auto mt-auto aspect-auto w-3/4">
+              <Button size="large" className="mt-6">
+                <a
+                  href="http://virtualbroker-inverfinsa.aunesa.com"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  {" "}
+                  Clientes
+                </a>
+              </Button>
+              <picture className="mx-auto mt-auto aspect-auto w-1/2 md:w-1/3">
                 <img
                   src="/logo-inverfin.webp"
                   alt="Logo de Inverfin"
@@ -100,7 +153,7 @@ export default function Navbar() {
                 />
               </picture>
             </motion.aside>
-          </>
+          </MotionConfig>
         )}
       </AnimatePresence>
     </header>
